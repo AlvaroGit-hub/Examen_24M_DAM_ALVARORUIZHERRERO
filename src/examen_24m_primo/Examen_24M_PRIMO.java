88607241877dcbeae2ddd4d7c5a11f42dbd093ba
Ceinmark <Ceinmark@DESKTOP-S79LJ55>
2021-03-24 11:21:52 +0100

    primera subida

---
+++ src/examen_24m_primo/Examen_24M_PRIMO.java
@@ -0,0 +1,10 @@
+package examen_24m_primo;
+
+public class Examen_24M_PRIMO {
+	public static void main(String args[]) {
+		
+		System.out.println("Buenos días Ceinmark, soy Álvaro Ruiz");
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+	}
+
+}
